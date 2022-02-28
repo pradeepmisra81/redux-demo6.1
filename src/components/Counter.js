39b340016ef7e48c4dc30ae0a079a9a1eb3f6bbc
pradeep <pradeep.misra@gmail.com>
2022-02-28 14:17:08 +0530

    Added increase by 5 action

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -9,6 +9,10 @@ const Counter = () => {
     dispatch({type: 'increment'});
   }
 
+  const increseHandler = () => {
+    dispatch({type: 'increse', amount: 5});
+  }
+
   const decrementHandler = () => {
     dispatch({type: 'decrement'});
   }
@@ -19,6 +23,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increseHandler}>Increse by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
     </div>
--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,12 @@ const counterReducer = (state = { counter: 0} ,action) => {
         }
     }
 
+    if(action.type === 'increse') {
+        return {
+            counter: state.counter + 5,
+        }
+    }
+
     if(action.type === 'decrement') {
         return {
             counter: state.counter - 1,
